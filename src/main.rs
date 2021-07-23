1ddbd4ca2dcfc1fdcdf997d841d1da25267ba1ad
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-07-23 16:14:44 -0400

    add two basic functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+fn get_version() -> u16 {
+    1000
+}
+
+fn usage() {
+    println!("tinymd, a markdown compiler written by Ryan Mercadante");
+    println!("Version {}", get_version());
+}
+
 fn main() {
-    println!("Hello, world!");
+    usage();
 }
